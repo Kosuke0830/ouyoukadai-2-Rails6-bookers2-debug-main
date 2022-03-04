ee74c3f06fdf402c234b0b0785f665947cdde101
EC2 Default User <ec2-user@ip-172-31-11-66.ec2.internal>
2022-03-04 12:16:52 +0000

    followsystem

--- app/controllers/bookcomments_controller.rb
+++ app/controllers/bookcomments_controller.rb
@@ -1,17 +1,17 @@
 class BookcommentsController < ApplicationController
-  
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.bookcomments.new(bookcomment_params)
     comment.book_id = book.id
-    book.save
+    comment.save
     redirect_to book_path(book)
   end
-  
+
   private
 
   def bookcomment_params
     params.require(:bookcomment).permit(:comment)
   end
-  
+
 end
