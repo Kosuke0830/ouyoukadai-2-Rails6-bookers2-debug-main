607d59908f732f4893809c19ecaced0bd390e0c9
kosuke0830 <taskforce0830@gmail.com>
2022-03-08 14:58:56 +0000

    seachs

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
-  get "search" => "searches#search"
+  get '/search', to: 'searches#search'
 
 end
